19c9311ea0de69736829ed3e3770dbe0fe6db0f4
joe-domer <jdomer@capstone-is.com>
2020-03-04 17:11:40 -0500

    Changed app text again slightly

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This has been changed on the Feature branch!
+          This has been changed on the Feature branch! then pushed to Master...
         </p>
         <a
           className="App-link"
